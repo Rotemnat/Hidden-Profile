f5f8125b2c0e55a611a15b540bfd69ec88e2773a
Rotemnat <124131145+Rotemnat@users.noreply.github.com>
2023-06-25 14:39:04 +0300

    Update __init__.py

--- share_information1121/__init__.py
+++ share_information1121/__init__.py
@@ -8,7 +8,7 @@ Pay for a random round for guessing the best option
 class C(BaseConstants):
     NAME_IN_URL = 'share_information1121'
     PLAYERS_PER_GROUP = 4
-    NUM_ROUNDS = 10
+    NUM_ROUNDS = 8
     ENDOWMENT = cu(50)
     OPTIMAL_OPTION = 'פרויקט D'
     DATA_TEMPLATE = __name__ + '/Infoset.html'
