f6d658fc00d083a1801373efc8babecbada338eb
Rotemnat <rotemnat@post.bgu.ac.il>
2023-06-29 09:25:27 +0300

    rotem 27.6

--- choice1221/__init__.py
+++ choice1221/__init__.py
@@ -66,16 +66,19 @@ def set_payoffs(group: Group):
     popular_other_vote = max(group.votes_for_option1,group.votes_for_option2,group.votes_for_option3,group.votes_for_option5)
     if group.votes_for_optimal > 2:
         group.group_wins = True
-        Player.payoff = C.PAYOFF
+        for p in players:
+            p.payoff = C.PAYOFF
     elif group.votes_for_optimal == 2:
         if group.votes_for_optimal > popular_other_vote:
             group.group_wins = True
-            Player.payoff = C.PAYOFF
+            for p in players:
+                p.payoff = C.PAYOFF
         else:
             group.group_wins = False
     else:
         group.group_wins = False
         Player.payoff = cu(0)
+
 
 # PAGES
 class MyPage(Page):
